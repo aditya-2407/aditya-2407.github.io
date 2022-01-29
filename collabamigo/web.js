dbaf04a1c244984ebe7caa467b5fee4176cad905
aditya-2407 <aditya.ag28@gmail.com>
2022-01-29 11:59:58 +0530

    stuff

--- collabamigo/web.js
+++
@@ -1,7 +0,0 @@
-const tog=document.querySelector('.toggle')
-const shc=document.querySelector('.showcase')
-
-tog.addEventListener('click', function(){
-    tog.classList.toggle('active')
-    shc.classList.toggle('active')
-})
